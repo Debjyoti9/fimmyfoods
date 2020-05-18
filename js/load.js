517a6e89beeb5dd5ac9a0db86ceb2e13b124d2f2
Burny99 <debjyotiroy927@gmail.com>
2020-05-18 20:45:35 +0530

    first one

---
+++ js/load.js
@@ -0,0 +1,9 @@
+var overlay = document.getElementById("overlay");
+
+window.addEventListener('load' , function()
+                       
+{
+overlay.style.display = 'none' ;
+
+}                      
+)
